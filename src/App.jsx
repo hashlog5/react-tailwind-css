f7557198e00ce66d45b8af8c462332e37364431f
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-11-06 15:54:12 -0800

    Food.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import HeadlineCards from './components/HeadlineCards';
+import Food from './components/Food';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <HeadlineCards />
+      <Food />
     </div>
   );
 };
