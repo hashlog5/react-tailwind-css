ab6ca206426fe76acc2804386bec5744d9d46a5c
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-11-06 15:02:06 -0800

    mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,24 @@
-import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
-import { BsFillCartFill } from 'react-icons/bs';
+import { useState } from 'react';
+
+import {
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineClose,
+  AiFillTag,
+} from 'react-icons/ai';
+import { BsFillCartFill, BsFillSaveFill } from 'react-icons/bs';
+import { TbTruckDelivery } from 'react-icons/tb';
+import { FaUserFriends, FaWallet } from 'react-icons/fa';
+import { MdFavorite, MdHelp } from 'react-icons/md';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
       {/* left side */}
       <div className='flex items-center'>
-        <div className='cursor-pointer'>
+        <div className='cursor-pointer' onClick={() => setNav(!nav)}>
           <AiOutlineMenu size={30} />
         </div>
 
@@ -35,6 +47,59 @@ const Navbar = () => {
         <BsFillCartFill size={20} className='mr-2' />
         Cart
       </button>
+
+      {/* mobile menu */}
+      {/* overlay */}
+      {nav ? (
+        <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+      ) : (
+        ''
+      )}
+
+      {/* side drawer menu */}
+      <div
+        className={
+          nav
+            ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'
+            : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'
+        }
+      >
+        <AiOutlineClose
+          size={30}
+          className='absolute right-4 top-4 cursor-pointer'
+          onClick={() => setNav(!nav)}
+        />
+
+        <h2 className='text-2xl p-4'>
+          Best <span className='font-bold'>Eats</span>
+        </h2>
+
+        <nav>
+          <ul className='flex flex-col p-4 text-gray-800'>
+            <li className='text-xl py-4 flex'>
+              <TbTruckDelivery size={25} className='mr-4' /> Orders
+            </li>
+            <li className='text-xl py-4 flex'>
+              <MdFavorite size={25} className='mr-4' /> Favorites
+            </li>
+            <li className='text-xl py-4 flex'>
+              <FaWallet size={25} className='mr-4' /> Wallet
+            </li>
+            <li className='text-xl py-4 flex'>
+              <MdHelp size={25} className='mr-4' /> Help
+            </li>
+            <li className='text-xl py-4 flex'>
+              <AiFillTag size={25} className='mr-4' /> Promotions
+            </li>
+            <li className='text-xl py-4 flex'>
+              <BsFillSaveFill size={25} className='mr-4' /> Best Ones
+            </li>
+            <li className='text-xl py-4 flex'>
+              <FaUserFriends size={25} className='mr-4' /> Invite Friends
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
