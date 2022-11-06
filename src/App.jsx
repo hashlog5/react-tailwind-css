d1d10271dec91b2068eb3b7b90fe0d8da2a5e8a3
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-11-06 15:25:41 -0800

    HeadlineCards.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import HeadlineCards from './components/HeadlineCards';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HeadlineCards />
     </div>
   );
 };
